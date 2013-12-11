187c25deb89cf99eb1ce5f39e1fb7b2d52a87f0a
Erik Rosen <er367@cam.ac.uk>
2013-12-11 01:14:06 +0100

    Created a main file,
    Added functionality to display numbers of dataset

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
 __author__ = 'dancecommander'
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import cPickle
+import gzip
+
+
+#load the dataset
+f = gzip.open('/Users/dancecommander/Programming/PyCharm Projects/Training Set/mnist.pkl.gz','rb')
+
+train_set, valid_set, test_set = cPickle.load(f)
+t = train_set[0]
+im = t[0]
+images = np.resize(im, (28, 28))
+for i in range(1, 20, 1):
+    im = t[i]
+    im = np.resize(im, (28, 28))
+    temp = np.append(images, im, 0)
+    images = temp
+
+
+plt.imshow(images)
+plt.gray()
+plt.show()
+
+
+
+
+f.close()
+
+
